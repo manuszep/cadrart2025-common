bce598c13bc36a944ece41d6d0da6add6dfb4427
Emmanuel Szep <manuszep@gmail.com>
2025-07-16 17:02:29 +0200

    Fix offer status enums to be string because that's how mysql saves them and the comparison was not working

--- src/lib/entity/offer.entity.ts
+++ src/lib/entity/offer.entity.ts
@@ -1,12 +1,12 @@
-import { ICadrartTeamMember } from './team-member.entity';
-import { ICadrartClient } from './client.entity';
-import { ICadrartJob } from './job.entity';
-import { ICadrartApiEntity } from './base.entity';
+import { ICadrartTeamMember } from "./team-member.entity";
+import { ICadrartClient } from "./client.entity";
+import { ICadrartJob } from "./job.entity";
+import { ICadrartApiEntity } from "./base.entity";
 
 export enum ECadrartOfferStatus {
-  STATUS_CREATED,
-  STATUS_STARTED,
-  STATUS_DONE
+  STATUS_CREATED = "0",
+  STATUS_STARTED = "1",
+  STATUS_DONE = "2",
 }
 
 export interface ICadrartOffer extends ICadrartApiEntity {
